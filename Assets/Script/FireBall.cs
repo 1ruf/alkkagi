0aa1e8c10649a317e559ebdc7715ae9d2280b21f
1_ruf <gyeol0411@naver.com>
2024-09-26 12:55:47 +0900

    AddToRepository
    add my game into this repository. for github upload

---
+++ Assets/Script/FireBall.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FireBall : MonoBehaviour
+    ,IPointerDownHandler
+    ,IDragHandler
+    ,IPointerUpHandler
+{
+    [SerializeField] private GameObject ball;
+    private Rigidbody2D _rigid;
+
+
+    private Vector2 startPos;
+    private Vector2 finishPos;
+    private Vector2 fireAngle;
+    private float fireVelocity;
+
+    private bool isDraging;
+    private void Awake()
+    {
+        _rigid = ball.GetComponent<Rigidbody2D>();
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isDraging = true;
+        startPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Input.mousePosition.z));
+        print("start:"+startPos);
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (isDraging)
+        {
+            finishPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Input.mousePosition.z));
+        }
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDraging = false;
+        fireAngle = (finishPos - startPos).normalized;
+        fireVelocity = Vector2.Distance(startPos,finishPos) * 15;
+        print("velocity:"+fireVelocity+",angle:"+fireAngle);
+        _rigid.AddForce(-fireAngle * fireVelocity);
+    }
+}
